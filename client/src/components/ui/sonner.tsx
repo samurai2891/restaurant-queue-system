5d45588180093a952b7a798be276d931630dc21c
samurai2891 <fxkensuke0512@gmail.com>
2026-01-08 17:53:09 +0900

    fix(ui): change toast position to top-right

--- client/src/components/ui/sonner.tsx
+++ client/src/components/ui/sonner.tsx
@@ -8,6 +8,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      position="top-right"
       style={
         {
           "--normal-bg": "var(--popover)",
